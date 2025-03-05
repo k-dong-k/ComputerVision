bc7b96600778239f19f193b33611346110cf1ff6
k-dong-k <dainaandy3507@gmail.com>
2025-03-05 10:54:49 +0900

    Update a_first.py

--- 1주차/a_first.py
+++ 1주차/a_first.py
@@ -7,6 +7,23 @@ img = cv.imread('soccer.jpg')
 if img is None:
     sys.exit('파일이 존재하지 않습니다.')
 
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # BGR 컬러 영상을 명암 영상으로 변환
+cv.imwrite('soccer_gray.jpg', gray)  # 영상을 파일에 저장
+
+# 명암 영상은 1채널이라서 np.hstack을 위해 3채널로 변환
+gray_3ch = cv.cvtColor(gray, cv.COLOR_GRAY2BGR)
+
+# 원본 이미지와 그레이스케일 이미지를 가로로 연결
+merged = np.hstack((img, gray_3ch))
+
+cv.imshow('Original & Grayscale', merged)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+print(type(img))
+print(img.shape)
+
 # cv.imshow('Image Display', img)
 
 """
@@ -28,21 +45,3 @@ cv.imshow('Color image', img)
 cv.imshow('Gray image', gray)
 cv.imshow('Gray image small',gray_small)
 """
-
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # BGR 컬러 영상을 명암 영상으로 변환
-cv.imwrite('soccer_gray.jpg', gray)  # 영상을 파일에 저장
-
-# 명암 영상은 1채널이라서 np.hstack을 위해 3채널로 변환
-gray_3ch = cv.cvtColor(gray, cv.COLOR_GRAY2BGR)
-
-# 원본 이미지와 그레이스케일 이미지를 가로로 연결
-merged = np.hstack((img, gray_3ch))
-
-cv.imshow('Original & Grayscale', merged)
-
-cv.waitKey(0)
-cv.destroyAllWindows()
-
-print(type(img))
-print(img.shape)
-
